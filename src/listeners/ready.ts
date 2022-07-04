e701f1bb0c9dcc73e186ee0a6251c1e883fad233
chsyan <charles5850@gmail.com>
2022-07-03 19:53:12 -0700

    Check nicknames on interval

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,11 +1,31 @@
+import handleMember from "../handle-member";
+import log from "../log";
 import { Client } from "discord.js";
 
-const ready = (client: Client): void => {
+const ready = async (client: Client) => {
   client.on("ready", async () => {
     if (!client.user || !client.application) {
       return;
     }
-    console.log("Logged in as " + client.user.tag);
+    log("Logged in as " + client.user.tag);
+
+    const names: JSON = require('../../names.json');
+    setInterval(async function () {
+      // Interval handle member
+      const memberManagers = client.guilds.cache.map(guild => guild.members);
+      for (const memberManager of memberManagers) {
+        for (const [key, _value] of Object.entries(names)) {
+          try {
+            const member = await memberManager.fetch(key);
+            await handleMember(member);
+          } catch (e: unknown) {
+            if (e instanceof Error && e.message !== "Unknown Member") {
+              log(e.message);
+            }
+          }
+        }
+      }
+    }, (10000));
   });
 };
 
